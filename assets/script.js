cef0eca52ed29985d2d8e20d38b25cb5f6cda6d4
Hads <112281105+hadscloud@users.noreply.github.com>
2022-10-22 10:34:35 -0700

    adding first js function and var

--- assets/script.js
+++ assets/script.js
@@ -16,25 +16,42 @@
     
 //}
 
-
-var currentDate = ""; // string for holding date index to timeEntries
-var currentDateString = ""; // string for holding today's date for display
-var currentHour = 9; // current hour for highlighting the correct row, default to first hour
-var timeEntries = []; // initialize list of log entries
-
-const timeEntriesName = "workDaySchedulerList"; // name used for localstorage
-const firstEntry = 9; // first displayed time block, relative to hourId (9AM)
-const lastEntry = 17; // last display time block, relative to hourId (5PM)
-const hourId = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM",
-                "1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"]; // map of military hours
+// string for holding date index to timeEntries
+var currentDate = ""; 
+// string for holding today's date for display
+var currentDateString = ""; 
+// current hour for highlighting the correct row, default to first hour
+var currentHour = 9; 
+// initialize list of log entries
+var timeEntries = []; 
+// name used for localstorage
+const timeEntriesName = "workDaySchedulerList";
+// first displayed time block, relative to hourId (9AM)
+const firstEntry = 9; 
+// last display time block, relative to hourId (5PM)
+const lastEntry = 17; 
+const hourId = ["12AM","1AM","2AM","3AM","4AM","5AM","6AM","7AM","8AM","9AM","10AM","11AM","12PM", "1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"];
+            
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", 
                 "July", "August", "September", "October", "November", "December"];
 
 setCurrentDateAndHour(); // Set currentDate, currentDateString, and currentHour, display date in header
-buildTimeBlocks(); // Build rest of html for page
-getTimeEntries(); // See if there are entries in localstorage and load them
 
-$(".saveBtn").click(saveClick); // Set event handler for all save buttons
+// See if there are entries in localstorage and load them
+getTimeEntries();
+// Set event handler for all save buttons
+$(".saveBtn").click(saveClick); 
+//set the current date in header 
+function setCurrentDateAndHour() {
+    var today = new Date();
+    var day = today.getDate();
+    var dayEnd = "th";
 
+    currentHour = today.getHours();
+
+    if (day < 9) {
+        currentDate = today.getFullYear
+    }
+}
